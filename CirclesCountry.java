73c6d36e93ee8dbf0b4ecc7a1518aed2bf7a6c5b
allaoc <angusli98@gmail.com>
2018-09-03 14:18:36 -0400

    optimized

--- CirclesCountry.java
+++ CirclesCountry.java
@@ -2,18 +2,12 @@ public class CirclesCountry {
     public int leastBorders(int[] x, int[] y, int[] r, 
                             int x1, int y1, int x2, int y2) {
 	int borders = 0;
-	int in1;
-	int in2;
+	boolean in1;
+	boolean in2;
 	for (int c=0;c<r.length;c++)
 	{
-		if (Math.sqrt(Math.pow((x1-x[c]),2)+Math.pow((y1-y[c]),2))<r[c]){
-			in1=1;}
-		else{
-			in1=0;}
-		if (Math.sqrt(Math.pow((x2-x[c]),2)+Math.pow((y2-y[c]),2))<r[c]){
-			in2=1;}
-		else{
-			in2=0;}
+		in1 = (Math.sqrt(Math.pow((x1-x[c]),2)+Math.pow((y1-y[c]),2))<r[c]);
+		in2 = (Math.sqrt(Math.pow((x2-x[c]),2)+Math.pow((y2-y[c]),2))<r[c]);
 		if (in1!=in2){
 			borders++;
 		}
